bf0aa73343e36a2e5e4ea724087b6bee462fb07d
Kyle Upton <kyleaupton@gmail.com>
2024-06-25 19:01:52 -0400

    added `argsTransform` for `ipcInvoke`

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,11 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ipcRenderer } from 'electron';
 import { IpcChannel } from './shared.js';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ArgsTransform = <P extends any[]>(...args: P) => P;
+
+/**
+ * A function that can be used to transform the arguments before sending them to the main process.
+ */
+export let argsTransform: ArgsTransform | undefined;
+
 export const ipcInvoke = async <P extends any[], R>(
   channelDefinition: IpcChannel<P, R>,
   ...args: P
 ) => {
+  if (argsTransform) {
+    args = argsTransform(...args);
+  }
+
   const result = await ipcRenderer.invoke(channelDefinition.name, ...args);
 
   if (result && typeof result === 'object' && result.error) {
