a7a9b843526c714cc907a84ea77161f50d0e3a9d
Kyle Upton <kyleaupton@gmail.com>
2024-06-25 19:06:02 -0400

    fix ability to set `argsTransform`

--- src/renderer.ts
+++ src/renderer.ts
@@ -7,7 +7,14 @@ type ArgsTransform = <P extends any[]>(...args: P) => P;
 /**
  * A function that can be used to transform the arguments before sending them to the main process.
  */
-export let argsTransform: ArgsTransform | undefined;
+let argsTransform: ArgsTransform | undefined;
+
+/**
+ * Sets a function that will be used to transform the arguments before sending them to the main process.
+ */
+export const setArgsTransform = (transform: ArgsTransform) => {
+  argsTransform = transform;
+};
 
 export const ipcInvoke = async <P extends any[], R>(
   channelDefinition: IpcChannel<P, R>,
