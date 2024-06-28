8d7ae39dfaeb204f429b120b8a50718feb1bcea9
Kyle Upton <kyleaupton@gmail.com>
2024-06-28 17:35:05 -0400

    removed `argsTransform`

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,28 +2,10 @@
 import { ipcRenderer } from 'electron';
 import { IpcChannel } from './shared.js';
 
-type ArgsTransform = <P extends any[]>(...args: P) => P;
-
-/**
- * A function that can be used to transform the arguments before sending them to the main process.
- */
-let argsTransform: ArgsTransform | undefined;
-
-/**
- * Sets a function that will be used to transform the arguments before sending them to the main process.
- */
-export const setArgsTransform = (transform: ArgsTransform) => {
-  argsTransform = transform;
-};
-
 export const ipcInvoke = async <P extends any[], R>(
   channelDefinition: IpcChannel<P, R>,
   ...args: P
 ) => {
-  if (argsTransform) {
-    args = argsTransform(...args);
-  }
-
   const result = await ipcRenderer.invoke(channelDefinition.name, ...args);
 
   if (result && typeof result === 'object' && result.error) {
